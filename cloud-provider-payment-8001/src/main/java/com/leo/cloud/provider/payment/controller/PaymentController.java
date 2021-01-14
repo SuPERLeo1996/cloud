f00ae4e4d7de7d8297bedd57efa83646ecba6da8
龚琳杰 <674340679@qq.com>
2021-01-14 21:43:20 +0800

    initial commit

--- cloud-provider-payment-8001/src/main/java/com/leo/cloud/provider/payment/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/leo/cloud/provider/payment/controller/PaymentController.java
@@ -6,9 +6,12 @@ import com.leo.cloud.provider.payment.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName PaymentController
@@ -24,6 +27,8 @@ public class PaymentController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private String serverPort;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -45,6 +50,19 @@ public class PaymentController {
         } else {
             return new CommonResult(444,"fail",null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*****element:" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 
--- cloud-provider-payment-8004/src/main/java/com/leo/cloud/provider/payment/PaymentApplication8004.java
+++ cloud-provider-payment-8004/src/main/java/com/leo/cloud/provider/payment/PaymentApplication8004.java
@@ -3,7 +3,6 @@ package com.leo.cloud.provider.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName PaymentApplication
@@ -11,13 +10,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @Author Leo
  * @Date 2020/12/26 16:09
  */
-@EnableEurekaClient
-@SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentApplication8001 {
+@SpringBootApplication
+public class PaymentApplication8004 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApplication8001.class,args);
+        SpringApplication.run(PaymentApplication8004.class,args);
     }
 
 }
